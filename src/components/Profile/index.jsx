dc0698dec76a6b0f3fdd91de2241655f026a0ac8
Jose Antonio Moral <joxe.bailen@gmail.com>
2017-02-18 11:01:53 +0100

    Creado componente Profile

---
+++ src/components/Profile/index.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import styles from './profile.css'
+
+class Profile extends Component {
+    render (){
+        return (
+            <div className={styles.root}> 
+                <img className={styles.avatar} src={this.props.picture} />
+                <span className={styles.name}>{this.props.displayName}</span>
+                <ul className={styles.data}>
+                    <li>
+                        <span className='fa fa-user'></span> {this.props.username}
+                    </li>
+                    <li>
+                        <span className='fa fa-envalope'> </span> {this.props.emailAddress}
+                    </li>
+                    <li>
+                        <span className='fa fa-map-marker'></span> {this.props.location}
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
