b27f19b7b843a45934358e33cbb17d490f401ddf
Jose Antonio Moral <joxe.bailen@gmail.com>
2017-02-19 11:26:26 +0100

    Cambiado a Header a un componente Stateless

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
 import styles from './header.css'
 
-class Header extends Component {
-  render(){
-    return (
+function Header () {
+  return(
       <header className={styles.root}>
         <h1 className={styles.logo}>Reacttr</h1>
-      </header>
-    )
-  }
+      </header> 
+      )
 }
 
 export default Header
